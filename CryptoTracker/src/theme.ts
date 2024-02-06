a2fdcaf8bc345ed638b0f69c1e378ba2520b8b1b
taewon <112356419+taewonnn@users.noreply.github.com>
2024-02-06 16:35:17 +0900

    feat: Theme

--- CryptoTracker/src/theme.ts
+++ CryptoTracker/src/theme.ts
@@ -1,13 +1,14 @@
 import { DefaultTheme } from 'styled-components/dist/types';
 
 export const theme: DefaultTheme = {
-  bgColor: 'white',
-  textColor: 'black',
-  btnColor: 'tomato',
+  bgColor: '#2f3640',
+  textColor: '#f5f6fa',
+  accentColor: '#4cd137',
 };
 
-export const darkTheme: DefaultTheme = {
-  bgColor: 'black',
-  textColor: 'white',
-  btnColor: 'teal',
-};
+// 다크모드 - 🛠️ 수정예정
+// export const darkTheme: DefaultTheme = {
+//   bgColor: 'black',
+//   textColor: 'white',
+//   accentColor: 'teal',
+// };
