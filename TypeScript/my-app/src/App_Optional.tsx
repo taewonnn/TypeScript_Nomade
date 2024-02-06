e60ed48fd18eaa49dad49529191eef875923444a
taewon <112356419+taewonnn@users.noreply.github.com>
2024-02-06 14:04:39 +0900

    optional Props

---
+++ TypeScript/my-app/src/App_Optional.tsx
@@ -0,0 +1,12 @@
+import Circle from './Circle';
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" />
+      <Circle text="im here" bgColor="tomato" borderColor="yellow" />
+    </div>
+  );
+}
+
+export default App;
