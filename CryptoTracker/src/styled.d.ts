9c10eda579092219f3ebeefb688d510777f7c228
taewon <112356419+taewonnn@users.noreply.github.com>
2024-02-06 15:07:21 +0900

    feat: project setting

---
+++ CryptoTracker/src/styled.d.ts
@@ -0,0 +1,11 @@
+// declare file
+
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    textColor: string;
+    bgColor: string;
+    btnColor: string;
+  }
+}
