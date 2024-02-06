5762be7ec7d4e47dda731933b59475f1e591e6c6
taewon <112356419+taewonnn@users.noreply.github.com>
2024-02-06 16:35:41 +0900

    feat: Theme

--- CryptoTracker/src/routes/Coins.tsx
+++ CryptoTracker/src/routes/Coins.tsx
@@ -1,9 +1,11 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
+
 function Coins() {
-  return (
-    <div>
-      <h1>Coins</h1>
-    </div>
-  );
+  return <Title>Coins</Title>;
 }
 
 export default Coins;
