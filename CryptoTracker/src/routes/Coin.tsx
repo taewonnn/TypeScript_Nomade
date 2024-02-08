f3641ff758f120c15b24f43890e279576cb2e87e
taewon <112356419+taewonnn@users.noreply.github.com>
2024-02-08 10:28:25 +0900

    feat - coin page

--- CryptoTracker/src/routes/Coin.tsx
+++ CryptoTracker/src/routes/Coin.tsx
@@ -1,18 +1,70 @@
-import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { useLocation, useParams } from 'react-router-dom';
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 20px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface ILocation {
+  state: {
+    name: string;
+  };
+}
 
 function Coin() {
-  // useParams() => url 뒤 파라미터 가져오기
+  // 💡useParams() => url 뒤 파라미터 가져오기
   // <Route path="/:coinId" element={<Coin />}></Route>  -> URL : http://localhost:3000/btc
   // console.log(params) 결과 : {coinId: 'btc'}
+
   // const params = useParams();
   // console.log(params);
 
-  const { coinId } = useParams();
+  // 💡 useLocation() => 현재 위치한 url object로 전달
+  // Ex.  http://localhost:3000/btc-bitcoin 접속 시 console에 아래처럼 나옴
+  // {
+  //   "pathname": "/btc-bitcoin",
+  //   "search": "",
+  //   "hash": "",
+  //   "state": {
+  //       "name": "Bitcoin",
+  //       "rank": 1
+  //   },
+  //   "key": "22z72l4x"
+  // }
 
+  // const location = useLocation();
+  // console.log(location);
+
+  const { state } = useLocation() as ILocation;
+
+  const [loading, setLoading] = useState(true);
   return (
-    <div>
-      <h1>Coin : {coinId}</h1>
-    </div>
+    <Container>
+      <Header>
+        <Title>{state?.name || 'Loading...'}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
   );
 }
 
