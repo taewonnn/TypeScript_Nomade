68d5adcb4271b087eb72b5d72e391c89a9248d1e
taewon <112356419+taewonnn@users.noreply.github.com>
2024-02-08 11:03:44 +0900

    feat : fetch coin data

--- CryptoTracker/src/routes/Coin.tsx
+++ CryptoTracker/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useLocation, useParams } from 'react-router-dom';
 
@@ -35,9 +35,11 @@ function Coin() {
   // 💡useParams() => url 뒤 파라미터 가져오기
   // <Route path="/:coinId" element={<Coin />}></Route>  -> URL : http://localhost:3000/btc
   // console.log(params) 결과 : {coinId: 'btc'}
-
   // const params = useParams();
-  // console.log(params);
+  // console.log(params.coinId);
+
+  // 파라미터에서 coinId 가져오기
+  const { coinId } = useParams();
 
   // 💡 useLocation() => 현재 위치한 url object로 전달
   // Ex.  http://localhost:3000/btc-bitcoin 접속 시 console에 아래처럼 나옴
@@ -51,13 +53,36 @@ function Coin() {
   //   },
   //   "key": "22z72l4x"
   // }
-
   // const location = useLocation();
   // console.log(location);
 
   const { state } = useLocation() as ILocation;
 
+  // 로딩 상태
   const [loading, setLoading] = useState(true);
+
+  // 코인 정보
+  const [info, setInfo] = useState({});
+  // 코인 가격
+  const [price, setPrice] = useState({});
+
+  // 코인 정보 + 가격 정보 가져오기
+  useEffect(() => {
+    (async () => {
+      // coin 정보
+      const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+      console.log('info : ', infoData);
+      // coin 가격 정보
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      console.log('price : ', priceData);
+
+      // data 담아주기
+      setInfo(infoData);
+      setPrice(priceData);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
@@ -69,3 +94,6 @@ function Coin() {
 }
 
 export default Coin;
+
+// nico 제작 - coin 정보
+// https://ohlcv-api.nomadcoders.workers.dev/?coinId=${coinId}
