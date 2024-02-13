388611835070c00ab6a1f8a779f445d66f2cefe4
taewon <112356419+taewonnn@users.noreply.github.com>
2024-02-13 11:23:25 +0900

    feat: createBrowserRouter

--- CryptoTracker/src/Router.tsx
+++ CryptoTracker/src/Router.tsx
@@ -1,16 +1,54 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import Coin from './routes/Coin';
 import Coins from './routes/Coins';
+import Chart from './routes/Chart';
+import Price from './routes/Price';
 
-function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Coins />}></Route>
-        <Route path="/:coinId" element={<Coin />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Coins />,
+  },
+  {
+    path: '/:coinId',
+    element: <Coin />,
+    children: [
+      {
+        path: 'chart',
+        element: <Chart />,
+      },
+      {
+        path: 'price',
+        element: <Price />,
+      },
+    ],
+  },
+]);
 
-export default Router;
+export default router;
+
+// 방법1. createBrowserRouter
+// Ex.
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//   },
+//   {
+//     path: 'product',
+//     element: <Product />,
+//     children: [{}, {}],
+//   },
+// ]);
+
+// 방법2. BrowserRouter 방식 - 구형 방식
+// function Router() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Coins />}></Route>
+//         <Route path="/:coinId" element={<Coin />}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
