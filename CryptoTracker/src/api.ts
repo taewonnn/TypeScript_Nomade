a436daf27399815be5935765eb8898b766d84baf
taewon <112356419+taewonnn@users.noreply.github.com>
2024-02-13 15:22:25 +0900

    feat: api - coin info / price

--- CryptoTracker/src/api.ts
+++ CryptoTracker/src/api.ts
@@ -1,13 +1,28 @@
 // React Qyery
 // 1.fetcher 함수 -> fetch promise를 return
 
+// BASE_URL
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
 // coin 목록
 export function fetchCoins() {
-  // async/await 방식
-  // const response = await fetch('https://api.coinpaprika.com/v1/coins');
-  // const json = await response.json;
-  // console.log(json);
-
   // promise 방식
-  return fetch('https://api.coinpaprika.com/v1/coins').then((response) => response.json());
+  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
+}
+
+// coin 정보 - 유료 API
+export function fetchCoinInfo(coinId: string | undefined) {
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) => response.json());
+}
+
+// coin 정보 - nomade API
+export function fetchCoinInfo2(coinId: string | undefined) {
+  return fetch(`https://ohlcv-api.nomadcoders.workers.dev/?coinId=${coinId}`).then((response) =>
+    response.json()
+  );
+}
+
+// coin 가격
+export function fetchCoinTickers(coinId: string | undefined) {
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) => response.json());
 }
