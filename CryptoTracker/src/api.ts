cef7c100756501fc258cc286cbe27d1075b46b36
TaeWon Park <thisis_tw94@naver.com>
2024-02-14 23:15:37 +0900

    chore

--- CryptoTracker/src/api.ts
+++ CryptoTracker/src/api.ts
@@ -31,12 +31,11 @@ export function fetchCoinInfo(coinId: string | undefined) {
 
 // coin tickers
 export function fetchCoinTickers(coinId: string | undefined) {
-  return fetch(`${BASE_URL}/${coinId}/tickers`)
-    .then((response) => response.json())
-    .then((json) => {
-      json.tickers = json.tickers.filter((ticker: any) => ticker.market.name === 'Binance');
-      return json;
-    });
+  return fetch(`${BASE_URL}/${coinId}/tickers`).then((response) => response.json());
+  // .then((json) => {
+  //   json.tickers = json.tickers.filter((ticker: any) => ticker.market.name === 'Binance');
+  //   return json;
+  // });
 }
 
 // coin 가격 기록
@@ -45,56 +44,3 @@ export function fetchCoinHistory(coinId: string | undefined) {
     response.json()
   );
 }
-
-/** 참고 */
-// const BASE_URL = `https://api.coinpaprika.com/v1`;
-// const CRYPTO_COMPARE_HIST_URL = `https://min-api.cryptocompare.com/data/v2/histoday?`;
-// const CRYPTO_COMPARE_PRICE_URL = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH&tsyms=USD`;
-
-// const NICO_API = `https://ohlcv-api.nomadcoders.workers.dev?coinId=`;
-
-// export function fetchCryptos() {
-// 	return fetch(`${BASE_URL}/coins`).then((response) => response.json());
-// }
-
-// export function fetchCryptosFromCoinGecko() {
-// 	return fetch(
-// 		`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1`
-// 	).then((response) => response.json());
-// }
-
-// export function fetchCryptoInfo(cryptoId: string) {
-// 	return fetch(
-// 		`https://api.coingecko.com/api/v3/coins/${cryptoId}?localization=false`
-// 	).then((response) => response.json());
-// }
-
-// export function fetchCryptoTickers(cryptoId: string) {
-// 	return fetch(`${BASE_URL}/tickers/${cryptoId}`).then((response) =>
-// 		response.json()
-// 	);
-// }
-
-// export function fetchMarketChart(cryptoId: string, from: string, to: string) {
-// 	return fetch(`https://api.coingecko.com/api/v3/coins/${cryptoId}/market_chart/range?vs_currency=usd&from=${from}&to=${to}
-// 	`);
-// }
-
-// export function fetchCoinHistory(cryptoId: string, days: string) {
-// 	//const endDate = Math.floor(Date.now() / 1000);
-// 	//const startDate = endDate - 60 * 60 * 24 * 7; //one week ago
-// 	//console.log("endDate: ", endDate, "startDate: ", startDate);
-// 	return fetch(
-// 		`https://api.coingecko.com/api/v3/coins/${cryptoId}/ohlc?vs_currency=usd&days=${days}
-// 		`
-// 	).then((response) => response.json());
-// }
-
-// export function fetchAllCoinPrice(cryptoSymbols: string | undefined) {
-// 	const link = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptoSymbols}&tsyms=USD`;
-// 	return fetch(
-// 		`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptoSymbols}&tsyms=USD`
-// 	).then((response) => response.json());
-// }
-
-//`${CRYPTO_COMPARE_URL}/?fsym=${cryptoSymbol}&tsym=USD&limit=20`
