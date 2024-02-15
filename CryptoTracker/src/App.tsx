24da08a6108582da87bcc17c5a227178212e1aaf
taewonnn <thisis_tw94@naver.com>
2024-02-15 15:55:06 +0900

    chore

--- CryptoTracker/src/App.tsx
+++ CryptoTracker/src/App.tsx
@@ -4,7 +4,6 @@ import { RouterProvider } from 'react-router-dom';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 import { lightheme, darkTheme } from './theme';
-import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { isDarkAtom } from './atoms';
 
