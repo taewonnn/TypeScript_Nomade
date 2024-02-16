c43bc881aabb4898a9305a93a01b48a2eea01be6
taewonnn <thisis_tw94@naver.com>
2024-02-16 11:40:11 +0900

    feat: recoil setting

--- TodoList/src/index.tsx
+++ TodoList/src/index.tsx
@@ -4,13 +4,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme } from './theme';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
 
