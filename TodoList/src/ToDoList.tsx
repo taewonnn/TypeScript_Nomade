afb3ed223c4e2bfb372251c050a4b5550e4bde98
taewonnn <thisis_tw94@naver.com>
2024-02-16 12:23:45 +0900

    feat: todo Form

--- TodoList/src/ToDoList.tsx
+++ TodoList/src/ToDoList.tsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+
 function ToDoList() {
+  // react-hook-form 사용 없이 Form 관리
+  const [toDo, setToDo] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDo(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('제출 : ', toDo);
+  };
   return (
-    <>
-      <div>todo</div>
-    </>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} onChange={onChange} type="text" placeholder="Write a to do" />
+        <button>Add</button>
+      </form>
+    </div>
   );
 }
 
