47079b3d57002176535975957919c46f7bb4343a
TaeWon Park <thisis_tw94@naver.com>
2024-02-18 12:48:20 +0900

    feat: useForm

--- TodoList/src/ToDoList.tsx
+++ TodoList/src/ToDoList.tsx
@@ -2,13 +2,16 @@ import { useForm } from 'react-hook-form';
 
 function ToDoList() {
   // react-hook-form 사용
-  const { register } = useForm();
-  console.log(register);
+  const { register, watch } = useForm();
+  // console.log(register('toDo')); // {name: 'toDo', onChange: ƒ, onBlur: ƒ, ref: ƒ}
 
+  // watch -> form 입력값들의 변화를 관찰할 수 있게 해주는 함수
+  console.log(watch('toDo')); // input에 내가 입력한 값을 콘솔에서 보여준다!
   return (
     <div>
       <form>
-        <input type="text" placeholder="Write a to do" />
+        {/* register -> input의 onchange onblur 이벤트를 대신해줌 즉, userState / onChage 함수를 손수 쓸 필요가 없음 */}
+        <input {...register('toDo')} type="text" placeholder="Write a to do" />
         <button>Add</button>
       </form>
     </div>
