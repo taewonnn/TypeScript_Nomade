7ab7b4cbef8f53e8b23ad978f059c6ce5e340b35
taewonnn <thisis_tw94@naver.com>
2024-02-19 11:49:59 +0900

    feat: setValue

--- TodoList/src/ToDoList.tsx
+++ TodoList/src/ToDoList.tsx
@@ -8,21 +8,30 @@ interface IForm {
 
 function ToDoList() {
   // react-hook-form 사용
-  const { register, watch, handleSubmit } = useForm<IForm>();
+  const { register, watch, handleSubmit, setValue } = useForm<IForm>();
 
   // input에 내가 입력한 값을 콘솔에서 보여준다!
   console.log(watch('toDo'));
 
   // 제출한 데이터 확인
   const handleValid = (data: IForm) => {
-    console.log('add to do : ', data.toDo);
+    console.log('제출 add to do : ', data.toDo);
+
+    // 제출하고 난 후 Input 깂 비워주기
+    setValue('toDo', '');
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit(handleValid)}>
         {/* register -> input의 onchange onblur 이벤트를 대신해줌 즉, userState / onChage 함수를 손수 쓸 필요가 없음 */}
-        <input {...register('toDo')} type="text" placeholder="Write a to do" />
+        <input
+          {...register('toDo', {
+            required: 'Please Write a To Do',
+          })}
+          type="text"
+          placeholder="Write a to do"
+        />
         <button>Add</button>
       </form>
     </div>
