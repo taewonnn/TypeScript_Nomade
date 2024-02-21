443fd781a2475aaf51de84518d37e8d8d35cb0a0
taewonnn <thisis_tw94@naver.com>
2024-02-21 18:04:51 +0900

    recoilprac

---
+++ RecoilPrac2/src/theme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from 'styled-components/dist/types';
+
+//다크모드
+export const darkTheme: DefaultTheme = {
+  bgColor: '#2f3640',
+  textColor: 'white',
+  accentColor: '#9c88ff',
+  cardBgColor: 'transparent',
+};
+
+export const lightheme: DefaultTheme = {
+  bgColor: 'whitesmoke',
+  textColor: 'black',
+  accentColor: '#9c88ff',
+  cardBgColor: 'white',
+};
