3a8ea9994b40b44902c1d8e3b957f3ff76f18d47
taewonnn <thisis_tw94@naver.com>
2024-02-21 17:25:15 +0900

    slector : minutes to hours

--- RecoilPrac/src/App.tsx
+++ RecoilPrac/src/App.tsx
@@ -1,5 +1,29 @@
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { hoursSelector, minuteState } from './atoms';
+import React from 'react';
+
 function App() {
-  return <></>;
+  /** minutes 값 / 값을 바꾸는 함수 가져오기 */
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+
+  /** hours 값 가져오기 */
+  const hours = useRecoilValue(hoursSelector);
+
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    // value는 문자열 / atom에서 minutes 기본값을 0 으로 해놔서 Number로 넘겨줘야한다.
+    // 💡 string을 number로 바꾸기 : +
+    // Ex.  +'1'  => 1
+    setMinutes(+value);
+  };
+  return (
+    <div>
+      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
+  );
 }
 
 export default App;
--- RecoilPrac/src/atoms.tsx
+++ RecoilPrac/src/atoms.tsx
@@ -3,3 +3,17 @@ import { atom, selector } from 'recoil';
 /** Interface Start */
 
 /** Interface End */
+
+export const minuteState = atom({
+  key: 'minutes',
+  default: 0,
+});
+
+/** state를 가져오고 output 수정 */
+export const hoursSelector = selector({
+  key: 'hours',
+  get: ({ get }) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+});
