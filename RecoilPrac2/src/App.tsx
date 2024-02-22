9db8df259f2cdf13a4aee29190e11cbccd651b77
taewonnn <thisis_tw94@naver.com>
2024-02-22 10:04:08 +0900

    react-beautiful-dnd 셋팅

--- RecoilPrac2/src/App.tsx
+++ RecoilPrac2/src/App.tsx
@@ -6,14 +6,22 @@ function App() {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="one">
-        {() => (
-          <ul>
+        {(magic) => (
+          <ul ref={magic.innerRef} {...magic.droppableProps}>
             <Draggable draggableId="first" index={0}>
-              {() => <li>One</li>}
+              {(magic) => (
+                <li ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
+                  One
+                </li>
+              )}
             </Draggable>
 
             <Draggable draggableId="second" index={1}>
-              {() => <li>Two</li>}
+              {(magic) => (
+                <li ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
+                  Two
+                </li>
+              )}
             </Draggable>
           </ul>
         )}
