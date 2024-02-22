a76995802aa9a3a1c0f589cf09f5bd44749019e6
taewonnn <thisis_tw94@naver.com>
2024-02-22 16:42:38 +0900

    memo

--- RecoilPrac2/src/Components/DraggableCad.tsx
+++ RecoilPrac2/src/Components/DraggableCad.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
@@ -18,6 +19,15 @@ interface IDraggableCardProps {
 /** Interface End */
 
 function DraggableCard({ toDo, index }: IDraggableCardProps) {
+  /** 렌더링 횟수 확인 */
+  /** components의 state가 바뀌면 모두 렌더링!
+  부모의 state가 바뀌면 부모의 자식들도 모두 다시 렌더링!
+
+  => react.memo  -> prop이 바뀌지 않는다면 굳이 렌더링 하지 말아줘!
+  */
+
+  console.log(toDo, 'has been rendered');
+
   return (
     <Draggable key={toDo} draggableId={toDo} index={index}>
       {(magic) => (
@@ -29,4 +39,5 @@ function DraggableCard({ toDo, index }: IDraggableCardProps) {
   );
 }
 
-export default DraggableCard;
+/** memo  -> prop이 바뀌지 않는다면 DraggableCard를 굳이 다시 렌더링 하지 말아줘! */
+export default memo(DraggableCard);
---
+++ RecoilPrac2/src/Components/index.js
@@ -0,0 +1,26 @@
+try {
+  var across_adn_contain = new fn_across_adn_contain(),
+    across_adn_param = [];
+  across_adn_param = { ut: 'Home', ui: '108268' };
+  across_adn_contain.init(across_adn_param);
+} catch (a) {}
+document.getElementById('preAdnPvTag') && document.getElementById('preAdnPvTag').remove();
+
+var adn1 =
+  '\u003Cscript id="preAdnPvTag" type="text/gtmscript"\u003Edocument.getElementById("adnPvTag")\u0026\u0026document.getElementById("adnPvTag").remove();\u003C/script\u003E\n\u003Cscript type="text/gtmscript" id="adnPvTag"\u003Etry{var across_adn_contain=new fn_across_adn_contain,across_adn_param=[];across_adn_param={ut:"Home",ui:"108268"};across_adn_contain.init(across_adn_param)}catch(a){}document.getElementById("preAdnPvTag")\u0026\u0026document.getElementById("preAdnPvTag").remove();\u003C/script\u003E\n';
+
+var adn2 =
+  '\u003Cscript id="preAdnCompTag1" type="text/gtmscript"\u003Edocument.getElementById("adnCompTag1")\u0026\u0026document.getElementById("adnCompTag1").remove();\u003C/script\u003E\n\u003Cscript type="text/gtmscript" id="adnCompTag1"\u003Etry{var across_adn_complete_contain=new fn_across_adn_contain,across_adn_complete_param=[];across_adn_complete_param={ut:"Complete",ui:"108268",uo:"types1"};across_adn_complete_contain.init(across_adn_complete_param)}catch(a){}document.getElementById("preAdnCompTag1")\u0026\u0026document.getElementById("preAdnCompTag1").remove();\u003C/script\u003E\n';
+
+
+
+
+  https://bigmom.co.kr/product/detail.html?product_no=9686&cate_no=117&display_group=1
+  https://bigmom.co.kr/product/detail.html?product_no=10614&cate_no=118&display_group=1
+  https://bigmom.co.kr/product/detail.html?product_no=7503&cate_no=32&display_group=2
+  https://bigmom.co.kr/product/detail.html?product_no=10697&cate_no=32&display_group=2
+  https://bigmom.co.kr/product/detail.html?product_no=10699&cate_no=31&display_group=2
+  https://bigmom.co.kr/product/detail.html?product_no=10700&cate_no=31&display_group=2
+  https://bigmom.co.kr/product/detail.html?product_no=10019&cate_no=117&display_group=1
+  https://bigmom.co.kr/product/detail.html?product_no=10696&cate_no=32&display_group=2
+
