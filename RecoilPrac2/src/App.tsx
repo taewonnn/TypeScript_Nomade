915252ed986a2711dcfec55e42e1b77d87a7e180
taewonnn <thisis_tw94@naver.com>
2024-02-22 09:48:37 +0900

    react-beautiful-dnd 셋팅

--- RecoilPrac2/src/App.tsx
+++ RecoilPrac2/src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 function App() {
-  return <div></div>;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="one">
+        {() => (
+          <ul>
+            <Draggable draggableId="first" index={0}>
+              {() => <li>One</li>}
+            </Draggable>
+
+            <Draggable draggableId="second" index={1}>
+              {() => <li>Two</li>}
+            </Draggable>
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 }
 
 export default App;
