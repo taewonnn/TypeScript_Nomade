14fe652a816f03c99abe268561af933a604efe9f
taewonnn <thisis_tw94@naver.com>
2024-02-23 12:03:48 +0900

    카드 옮길 때 style

--- RecoilPrac2/src/Components/DraggableCad.tsx
+++ RecoilPrac2/src/Components/DraggableCad.tsx
@@ -7,8 +7,8 @@ const Card = styled.div<{ isDragging: boolean }>`
   border-radius: 5px;
   margin-bottom: 5px;
   padding: 10px 10px;
-  background-color: ${(props) => (props.isDragging ? 'tomato' : props.theme.cardColor)};
-  box-shadow: ${(props) => (props.isDragging ? '0px 2px 5px rgba(0, 0, 0, 0, 1)' : 'none')};
+  background-color: ${(props) => (props.isDragging ? '#74b9ff' : props.theme.cardColor)};
+  box-shadow: ${(props) => (props.isDragging ? '0px 2px 25px rgba(0, 0, 0, 0.5)' : 'none')};
 `;
 /** Style End */
 
