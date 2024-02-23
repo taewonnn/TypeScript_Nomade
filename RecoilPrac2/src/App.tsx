4e6e0379dbcd5eb2cd69c2b30cc4f172fbb4a47e
taewonnn <thisis_tw94@naver.com>
2024-02-23 11:38:44 +0900

    style

--- RecoilPrac2/src/App.tsx
+++ RecoilPrac2/src/App.tsx
@@ -8,8 +8,7 @@ import Board from './Components/Board';
 /** Style Start */
 const Wrapper = styled.div`
   display: flex;
-  max-width: 680px;
-  width: 100%;
+  width: 100vw;
   margin: 0 auto;
   justify-content: center;
   align-items: center;
@@ -17,10 +16,11 @@ const Wrapper = styled.div`
 `;
 
 const Boards = styled.div`
-  display: grid;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
   width: 100%;
   gap: 10px;
-  grid-template-columns: repeat(1, 1fr);
 `;
 /** Style End */
 
