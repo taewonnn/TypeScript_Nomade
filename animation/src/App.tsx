18d679281bd9764fc4941c55678d7d7db03c13c4
TaeWon Park <thisis_tw94@naver.com>
2024-02-24 12:10:50 +0900

    animation - Ex2

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -15,8 +15,10 @@ const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 15px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: repeat(2, 1ft);
 `;
 
 const Circle = styled(motion.div)`
@@ -24,6 +26,7 @@ const Circle = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   height: 70px;
   border-radius: 35px;
+  place-self: center;
 `;
 /**Style End */
 
