e5c933d1bcd8886fd527b995c9d17efbf008fdde
taewonnn <thisis_tw94@naver.com>
2024-02-26 11:32:12 +0900

    feat: router 셋팅

--- Nomflix/src/App.tsx
+++ Nomflix/src/App.tsx
@@ -1,12 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Routes/home';
+import Tv from './Routes/Tv';
+import Search from './Routes/Search';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/"></Route>
-        <Route path="/tv"></Route>
-        <Route path="/search"></Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
       </Routes>
     </Router>
   );
