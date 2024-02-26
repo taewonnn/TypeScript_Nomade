3d7c42097a4dba1140c547c34ff88a69a54f17d8
taewonnn <thisis_tw94@naver.com>
2024-02-26 11:24:39 +0900

    feat: 프로젝트 셋팅

---
+++ Nomflix/src/styled.d.ts
@@ -0,0 +1,18 @@
+// declare file
+
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    red: string;
+    black: {
+      veryDark: string;
+      darker: string;
+      lighter: string;
+    };
+    white: {
+      darker: string;
+      lighter: string;
+    };
+  }
+}
