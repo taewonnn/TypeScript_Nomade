aa542cceabea56ac1d55de9827442fd92f795b18
TaeWon Park <thisis_tw94@naver.com>
2024-02-27 21:58:03 +0900

    feat: movie data + style

--- Nomflix/src/Routes/Home.tsx
+++ Nomflix/src/Routes/Home.tsx
@@ -6,6 +6,31 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   background-color: black;
 `;
+
+const Loader = styled.div`
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
 /** Style End */
 
 function Home() {
@@ -18,7 +43,20 @@ function Home() {
   console.log(data, isLoading);
   /** 상영중인 영화 가져오기 */
 
-  return <Wrapper style={{ height: '200vh' }}></Wrapper>;
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
