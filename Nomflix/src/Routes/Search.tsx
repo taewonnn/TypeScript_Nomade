0fd0b78523fbc6e0576d281a4d56d452e5eefd59
taewonnn <thisis_tw94@naver.com>
2024-03-04 10:42:37 +0900

    feat: 검색 페이지 url 확인

--- Nomflix/src/Routes/Search.tsx
+++ Nomflix/src/Routes/Search.tsx
@@ -3,7 +3,8 @@ import { useLocation } from 'react-router-dom';
 function Search() {
   /** useLoaction - url 정보 */
   const location = useLocation();
-  console.log(location);
+  const keyword = new URLSearchParams(location.search).get('keyword');
+  console.log('keyword 가져오기 : ', keyword);
   /** useLoaction */
 
   return <div>Search</div>;
