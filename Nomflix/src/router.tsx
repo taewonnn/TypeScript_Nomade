a3940ea0b80f0c6f19240114df06a917bd4ca4ed
taewonnn <thisis_tw94@naver.com>
2024-03-04 13:41:47 +0900

    feat: 파일구조 변경

--- Nomflix/src/router.tsx
+++ Nomflix/src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Home from './Routes/Home/Home';
-import Tv from './Routes/Tv';
+import Tv from './Routes/Tv/Tv';
 
 import Search from './Routes/Search/Search';
 
