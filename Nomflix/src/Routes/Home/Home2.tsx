853f48529d3c416112c7862784142a0edbe7276a
taewonnn <thisis_tw94@naver.com>
2024-03-06 10:23:23 +0900

    feat: 유명한 영화 데이터 추가

--- Nomflix/src/Routes/Home/Home2.tsx
+++ Nomflix/src/Routes/Home/Home2.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useMatch, useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import * as Style from './Home.styles';
-import { IGetMovieResult, getMovies } from '../../api';
+import { IGetMovieResult, getMovies, getPopularMovie } from '../../api';
 import { makeImagePath } from '../../utils';
 import { AnimatePresence } from 'framer-motion';
 import useWindowDimensions from '../Components/useWindowDimensions';
@@ -54,7 +54,7 @@ function Home2() {
 
   /** useMatch('확인하고자 하는 param') */
   const movieIdMatch = useMatch('/movies/:movieId');
-  console.log('!!! movieId 확인', movieIdMatch?.params); // {movieId: '931642'}
+  // console.log('!!! movieId 확인', movieIdMatch?.params); // {movieId: '931642'}
   /** useMatch('확인하고자 하는 param') */
 
   /** 애니메이션 축 좌표 */
@@ -62,24 +62,32 @@ function Home2() {
   /** 애니메이션 축 좌표 */
 
   /** 상영중인 영화 가져오기 */
-  console.log('render!!');
-  const { data, isLoading } = useQuery<IGetMovieResult>({
-    queryKey: ['movies', 'nowPlaying'],
+  // console.log('render!!');
+  const { data: nowPlayingMovie, isLoading: NowPlayingIsLoading } = useQuery<IGetMovieResult>({
+    queryKey: ['nowPlaying'],
     queryFn: getMovies,
   });
-  console.log('상영중인 영화 data 확인 :', data);
+  // console.log('상영중인 영화 data 확인 :', data);
   /** 상영중인 영화 가져오기 */
+
+  /** 유명한 영화 가져오기 */
+  const { data: popularMovie } = useQuery({
+    queryKey: ['popularMovie'],
+    queryFn: getPopularMovie,
+  });
+  console.log('Popular:', popularMovie);
+  /** 유명한 영화 가져오기 */
 
   /** 슬라이드 - index */
   const [index, setIndex] = useState(0);
   const [leaving, setLeaving] = useState(false);
   // index 증가 함수
   const increaseIndex = () => {
-    if (data) {
+    if (nowPlayingMovie) {
       if (leaving) return;
       setLeaving(true);
       // 1개는 상단에 사용 중이라 -1
-      const totalMovies = data.results.length - 1;
+      const totalMovies = nowPlayingMovie.results.length - 1;
       const maxIndex = Math.floor(totalMovies / offset) - 1;
       setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
     }
@@ -103,30 +111,30 @@ function Home2() {
   /** 클릭한 box의 영화 정보 */
   const clickedMovie =
     movieIdMatch?.params.movieId &&
-    data?.results.find((movie) => movie.id + '' === movieIdMatch.params.movieId);
+    nowPlayingMovie?.results.find((movie) => movie.id + '' === movieIdMatch.params.movieId);
   console.log('click 영화 정보 ', clickedMovie);
   /** 클릭한 box의 영화 정보 */
 
   return (
     <Style.Wrapper>
-      {isLoading ? (
+      {NowPlayingIsLoading ? (
         <Style.Loader>Loading...</Style.Loader>
       ) : (
         <>
           <Style.Banner
             onClick={increaseIndex}
-            bgPhoto={makeImagePath(data?.results[0].backdrop_path || '')}
+            bgPhoto={makeImagePath(nowPlayingMovie?.results[0].backdrop_path || '')}
           >
-            <Style.Title>{data?.results[0].title}</Style.Title>
-            <Style.Overview>{data?.results[0].overview}</Style.Overview>
+            <Style.Title>{nowPlayingMovie?.results[0].title}</Style.Title>
+            <Style.Overview>{nowPlayingMovie?.results[0].overview}</Style.Overview>
           </Style.Banner>
           {/* 슬라이드 영역 - 첫번째 줄 */}
           <Style.Slider>
             <MovieSlider
               toggleLeaving={toggleLeaving}
-              width={window.innerWidth}
+              width={width}
               index={index}
-              data={data}
+              data={nowPlayingMovie}
               offset={6}
               onBoxClicked={onBoxClicked}
               BoxVariants={BoxVariants}
