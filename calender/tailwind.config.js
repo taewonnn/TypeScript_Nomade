de567816068d5b36e4702937b99ef40c03f14bc1
taewonnn <thisis_tw94@naver.com>
2024-03-06 16:50:29 +0900

    feat: theme color

--- calender/tailwind.config.js
+++ calender/tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        purple: '#3f3cbb',
+        midnight: '#121063',
+        metal: '#565584',
+        tahiti: '#3ab7bf',
+        silver: '#ecebff',
+        'bubble-gum': '#ff77e9',
+        bermuda: '#78dcca',
+      },
+    },
   },
   variants: {
     extend: {},
