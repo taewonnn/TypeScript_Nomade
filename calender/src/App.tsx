e71af5525c18bf1ad7407717974d8b10b12cef02
taewonnn <thisis_tw94@naver.com>
2024-03-06 13:53:55 +0900

    feat: fullcalendar 적용

--- calender/src/App.tsx
+++ calender/src/App.tsx
@@ -1,8 +1,15 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
 function App() {
   return (
-    <div className="App">
-      <p>Edit and save to reload.</p>
-    </div>
+    <>
+      <div className="text-center bg-blue-500"></div>
+      {/* FullCalendar */}
+      <div className="w-full h-1/2">
+        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      </div>
+    </>
   );
 }
 
